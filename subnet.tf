f5f10738fd7b2c7fbe58edbade68c1ebfcad33a3
Ahmed Abd Elhares <ahmedabdelharse@gmail.com>
2024-09-07 20:36:41 +0300

    update subnet

--- subnet.tf
+++ subnet.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "tf_public_subnet" {
 resource "aws_subnet" "tf_private_subnet" {
   vpc_id            = aws_vpc.tf_lab1_vpc.id
   cidr_block        = var.cidr_blocks[2].cidr_block
-  availability_zone = "eu-west-3a"
+  availability_zone = var.a_z
   tags = {
     Name = var.cidr_blocks[2].name
   }
